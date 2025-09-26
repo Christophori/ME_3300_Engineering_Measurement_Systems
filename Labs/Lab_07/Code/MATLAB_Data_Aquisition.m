68a62f66051be15846d6a29e27b5af5ddc740111
Rodrigo Padilla <33562715+engineerrod@users.noreply.github.com>
2025-09-26 15:37:22 -0700

    Updated Lab 6

--- Labs/Lab_07/Code/MATLAB_Data_Aquisition.m
+++ Labs/Lab_07/Code/MATLAB_Data_Aquisition.m
@@ -1,6 +1,6 @@
 % This is a generic program to aquire the data from myDAQ
-% Sample data acquisition program for collecting the data to measure
-% various amplifiers
+% Sample data acquisition program for collecting the data to capture
+% pendulum motion file for later use
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Date: 09/02/2025
 % Author: Dr. Christopher Bitikofer
@@ -21,17 +21,20 @@ Fs = 200; % Sampling rate data per sec
 T = 5; % Time to aquire data
 % Here change the device name and input channel as needed; if not sure then talk to TAs
 deviceName = 'myDAQ1';
-inputChannels = [0]; % use to sample a single channel
+inputChannels = 0; % use to sample a single channel
 enableLogging = true; % enable / disable logging to file (true/false)
 enablePlotting = true; % enable / disable plotting on a figure during aquisition (true/false)
 
 %% Input the filename and directory where to store the data
-filename = '../Data/LM358Data.csv'; % Name/ location for storing data file
+filename = '../Data/Student_Name_VoltsVsTime.csv'; % Name/ location for storing data file
 fid1 = fopen(filename,'w'); % this will fail if you don't create a blank csv in the required location first!
-vheader = ["Times (s)","Input Voltage (v)","Output Voltage (v)"]; % set the column titles for the data file
+% NOTE: If you use this make sure to change the name of your file
 
-plotTitle = 'Firstname LastName''s plot - Lab #06';
-fprintf(fid1,'%s,%s,%s\n',header(1),header(2),header(3)); % modify this line as needed for more columns in later labs
+% Uncomment for single-channel data aquisition
+header = ["Times (s)","Voltage (Volts)"]; % set the column titles for the data file
+
+plotTitle = 'Firstname LastName''s plot - Lab #03';
+fprintf(fid1,'%s,%s\n',header(1),header(2)); % modify this line as needed for more columns in later labs
 
 %% End of user information
 d = daq.getDevices;
@@ -39,21 +42,12 @@ s = daq.createSession('ni');
 addAnalogInputChannel(s,deviceName,[inputChannels],'Voltage');
 s.Rate = Fs; % Sample rate modify as required
 s.DurationInSeconds = T; % Sampling time modify as required
-
-% Create analog output for varying voltage
-addAnalogOutputChannel(s,deviceName,0,'Voltage');
-s.Rate = Fs;
-outputSignal1 = linspace(0,5,Fs*T)';
-queueOutputData(s,outputSignal1);
-
 if enablePlotting == 1
     lh1 = addlistener(s,'DataAvailable',@(src, event)plotData(src,event));
 end
-
 if enableLogging
     lh2 = addlistener(s,'DataAvailable',@(src, event)logData(src,event,fid1));
 end
-
 errorListener = addlistener(s, 'ErrorOccurred',@(src, event) disp(getReport(event.Error)));
 drawnow
 s.IsContinuous = true;
@@ -68,18 +62,16 @@ fclose(fid1)
 function logData(src,evt, fid)
 %Add the time stamp and the data values to data. To write data sequentially
 %transpose the matrix
-    data = [evt.TimeStamps evt.Data(:,1) evt.Data(:,2)];
-    fprintf(fid,'%f,%f.%f \n', data');
+data = [evt.TimeStamps m*evt.Data+c];
+fprintf(fid,'%f,%f \n', data');
 end
 
 %% Function to plot the data as it is being acquired
 function plotData(src,event)
-    time = event.TimeStamps;
-    inputvoltage = event.Data(:,1); % Amplified voltage
-    outputvoltage = event.Data(:,2); % Output voltage 0-5V
-    plot(time, InputData,'k-');hold on
-    plot(time, OutputData,'r-');
-    xlabel('time (s)')
-    ylabel('Voltage(v)')
-    legend('Input volt','Output volt','location','northwest')
+time = event.TimeStamps;
+voltage = event.Data; %Potentiometer data in voltage
+figure(1)
+plot(time, voltage,'k.-');hold on
+xlabel('time (s)')
+ylabel('Voltage (Volts)')
 end

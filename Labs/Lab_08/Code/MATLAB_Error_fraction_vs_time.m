9fc6233513735cd37cf993c13e497c2e3a312aff
Rodrigo Padilla <33562715+engineerrod@users.noreply.github.com>
2025-10-20 11:56:11 -0700

    Lab_08 Updates

---
+++ Labs/Lab_08/Code/MATLAB_Error_fraction_vs_time.m
@@ -0,0 +1,42 @@
+clear all
+close all
+clc
+
+
+%% User input %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../Data/TimeSeries_Temperature01.dat'); %Reading data from the stored file
+istr = 1562; iend = 1647;% Index of T_0 and T_ss
+
+%% End of user input %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+line = fgetl(fid);
+data = fscanf(fid,'%f \n', [2 inf]);
+time = data(1,istr:iend) - data(1,istr);
+temperature = data(2,istr:iend);
+fclose(fid);
+Tss = 98.0; % Steady State value from thermometer
+Y2 = log((temperature-Tss)./(temperature(1)-Tss)); %Error fraction operation
+%% Performing Regression Analysis
+[p,s] = polyfit (time,Y2,1); %curve fitting with 1st order fit
+xfit = time;%Generating degree of freedom for the curve fit
+yfit = polyval(p,xfit);
+
+% Generating figure with specific size
+figure (1)
+set(gcf,'unit','inches','position',[0.50 0.50 6.50 3.50],...
+    'defaultaxesfontsize',10,'defaultaxesfontname','times');
+% Plotting data
+plot(time,Y2,'ro','markersize',4,'markerfacecolor','r');hold on
+plot(xfit,yfit,'b-','linewidth',2);
+xlim([0 0.06])
+%ylim([0 10])
+xlabel('\Gamma (Error fraction)')
+ylabel('Time (s)')
+legend('Curve fit','Expt. data','location','northeast')
+text(0.01,-1.4,sprintf('%s%3.4f%s','\tau=',-1/p(1),'s'))
+grid on
+grid minor
+title('Student''s Name Error Fraction plot')
+
+%% Saving the files in png and pdf format
+exportgraphics(better_fig,"..\Figures\My_Awesome_Error_Fraction_Plot.png",Resolution=600) % for reports/presentations
+exportgraphics(better_fig,"..\Figures\My_Awesome_Error_Fraction_Plot.pdf",Resolution=600) % for canvas submission 
---
+++ Labs/Lab_08/Code/MATLAB_Temp_vs_time_plotting.m
@@ -0,0 +1,53 @@
+clear all
+close all
+clc
+
+
+%% User input %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+t_0 = [1.561, 1.415, 1.484];
+
+%% End of user input %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../Data/TimeSeries_Temperature01.dat'); %Reading data from the stored file
+line1 = fgetl(fid);
+data1 = fscanf(fid,'%f \n', [2 inf]);
+time1 = data1(1,:)- t_0(1);
+temperature1 = data1(2,:);
+fclose(fid);
+
+fid = fopen('../Data/TimeSeries_Temperature02.dat'); %Reading data from the stored file
+line1 = fgetl(fid);
+data2 = fscanf(fid,'%f \n', [2 inf]);
+time2 = data2(1,:) - t_0(2);
+temperature2 = data2(2,:);
+fclose(fid);
+
+fid = fopen('../Data/TimeSeries_Temperature03.dat'); %Reading data from the stored file
+line1 = fgetl(fid);
+data3 = fscanf(fid,'%f \n', [2 inf]);
+time3 = data3(1,:) - t_0(3);
+temperature3 = data3(2,:);
+fclose(fid);
+
+%% Generating figure with specific size
+figure (1)
+set(gcf,'unit','inches','position',[0.50 0.50 6.50 3.50],...
+    'defaultaxesfontsize',10,'defaultaxesfontname','times');
+%Plotting data
+plot(time1,temperature1,'ro','markersize',3,'markerfacecolor','r');hold on
+plot(time2,temperature2,'bo','markersize',3,'markerfacecolor','b')
+plot(time3,temperature3,'ko','markersize',3,'markerfacecolor','k')
+xlim([-.1 0.5])
+ylim([20 100])
+text(0.3,70,'\tau_{1} = 0.0 s', 'fontname','times') %Change tau value
+text(0.3,67,'\tau_{2} = 0.0 s', 'fontname','times') %Change tau value
+text(0.3,64,'\tau_{3} = 0.0 s', 'fontname','times') %Change tau value
+text(0.3,61,'\tau_{avg} = 0.0 s', 'fontname','times') %Change average tau value
+xlabel('Time(t)')
+ylabel('Temperature (T)')
+legend('Run 01','Run 02','Run 03','location','northwest')
+grid on
+title('Student''s Name Time Series Plot')
+
+%% Saving the files in png and pdf format
+exportgraphics(better_fig,"..\Figures\My_Awesome_Time_Series_Plot.png",Resolution=600) % for reports/presentations
+exportgraphics(better_fig,"..\Figures\My_Awesome_Time_Series_Plot.pdf",Resolution=600) % for canvas submission 

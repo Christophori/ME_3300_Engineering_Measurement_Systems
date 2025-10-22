0a369a2469b72ba1369d961464482fd6290a8346
Rodrigo Padilla <33562715+engineerrod@users.noreply.github.com>
2025-10-21 17:13:31 -0700

    Slope and bias removed

--- Labs/Lab_07/Code/MATLAB_Data_Aquisition.m
+++ Labs/Lab_07/Code/MATLAB_Data_Aquisition.m
@@ -62,7 +62,7 @@ fclose(fid1)
 function logData(src,evt, fid)
 %Add the time stamp and the data values to data. To write data sequentially
 %transpose the matrix
-data = [evt.TimeStamps m*evt.Data+c];
+data = [evt.TimeStamps evt.Data];
 fprintf(fid,'%f,%f \n', data');
 end
 
